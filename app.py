42755a69768767a8b9f2fa83452b1a1a356f502b
Roy Prins <prinsroy@gmail.com>
2017-09-19 14:41:47 +0200

    Static site generator

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+app.config['FREEZER_DESTINATION'] = 'gh-pages'
+app.config['FREEZER_DESTINATION_IGNORE'] = ['.git*', 'CNAME', '.gitignore', 'readme.md']
+app.config['FREEZER_RELATIVE_URLS'] = True
+
+HOSTING_DOMAIN = 'http://mdejongere.nl/'
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/contact.html")
+def contact():
+    return render_template('contact.html')
+
+
+@app.route("/ingeschreven.html")
+def signed_up():
+    return render_template('ingeschreven.html')
+
+
+@app.route("/uitgeschreven.html")
+def signed_off():
+    return render_template('uitgeschreven.html')
+
+
+@app.route("/404.html")
+def not_found():
+    return render_template('404.html')
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return render_template('sitemap.xml')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
